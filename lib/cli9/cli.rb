36d659d5974788d134da8524fc1b4667f7d7b55b
Shayan Mohsin <shaymotionfilms@gmail.com>
2019-10-14 20:02:17 +0000

    changes

--- lib/cli9/cli.rb
+++ lib/cli9/cli.rb
@@ -1,6 +1,7 @@
 class GuitarGallery::CLI
   def call
     puts "\nWelcome To The Guitar Gallery\n"
+    puts ""
   get_guitars
   list_guitars
   get_user_guitar
@@ -12,44 +13,51 @@ class GuitarGallery::CLI
   end
   
   def list_guitars
-     @guitars.all.each.with_index(1) do |guitar, index|
+     @guitars.each.with_index(1) do |guitar, index|
         puts "#{index}. #{guitar.name}" 
      end
   end
 
   def get_user_guitar
+    puts ""
     puts "Select A Guitar By Entering a Number From 1-10."
     chosen_guitar = gets.strip
+    
     if valid_input(chosen_guitar.to_i, @guitars) 
-      show_guitars_for(chosen_guitar)
+      show_guitar(chosen_guitar)
     end
   end
 
   def valid_input(input, data)
-    input.to_i <= data.length && input.to_i < 0  
+     #binding.pry
+    input.to_i <= data.length && input.to_i > 0 
   end
 
-  def show_guitars_for(chosen_guitar)
-    guitar = @guitars[chosen_guitar - 1]
+  def show_guitar(chosen_guitar)
+    guitar = @guitars[chosen_guitar.to_i - 1]
+    puts ""
     puts "Excellent Choice! Rock on!"
-    guitars.Custom_Guitars.each.with_index(1) do |guitar, idx|
-      puts "#{idx}. #{guitar.name}"
-      puts #{guitar.rating}
-      puts #{guitar.summary}
-    end
+    puts ""
+      puts guitar.name
+      puts guitar.rating
+      puts guitar.summary
+      learn_more
   end
 
-  def learn_more(guitar)
+  def learn_more
+    puts ""
     puts "Check Out Another Guitar(Y/N)?"
+    puts ""
     input = "nil"
     until input == "Y" || input == "N"
      puts "type Y(yes) or N(no) "
      input = gets.strip.upcase
     end
-    if input == "y"
+    if input == "Y"
      puts "Select Again"
-     list_guitars
-    elsif input == "n"
+     call
+    elsif input == "N"
+    puts ""
      puts "Rock on! Goodbye."
     end
   end
--- lib/cli9/scraper.rb
+++ lib/cli9/scraper.rb
@@ -1,22 +1,22 @@
 class GuitarGallery::Scraper
 
   def self.scrape_guitars
-  site = "https://www.guitarfella.com/best-electric-guitar/"
+   site = "https://www.guitarfella.com/best-electric-guitar/"
 
-  page = Nokogiri::HTML(open(site))
-  results = page.css(".best_lists_table tr")
+   page = Nokogiri::HTML(open(site))
+   results = page.css(".best_lists_table tr")
 
 
   results.drop(1).each do |r|
-  #puts r
-  name = r.css('.table_product_name a').text.strip
-  rating = r.css('td:nth-child(2) p').text.strip
-  summary = r.css('td:nth-child(3) p').text.strip
-  guitar = GuitarGallery.new(name, rating, summary)
-  puts guitar.name
-  puts guitar.rating
-  puts summary
-    end
+    #puts r
+    name = r.css('.table_product_name a').text.strip
+    rating = r.css('td:nth-child(2) p').text.strip
+    summary = r.css('td:nth-child(3) p').text.strip
+  guitar = GuitarGallery::Guitar.new(name, rating, summary)
+   #puts guitar.name
+   #puts guitar.rating
+   #puts summary
+   end
   end
   
 end  
